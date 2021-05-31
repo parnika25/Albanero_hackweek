11127d49a73bae1c5ed635de8c2cefaad8a60d06
Parnika Katiyar <59792325+parnika25@users.noreply.github.com>
2021-06-01 03:51:05 +0530

    Update q8.java

--- Day_1/q8.java
+++ Day_1/q8.java
@@ -1,34 +1,33 @@
 import java.util.*;
-public class q8 
+public class q8
 {
 
-    static int largest_sum(int arr[],int n,int k)
-    {
-        int sum=0;
-        for(int i=1;i<=k;i++)
-        {
-            int min=arr[0];
-            int index=0;
-            for(int j=1;j<n;j++)
-            {
-                if(arr[j]<min)
-                {
-                    min=arr[j];
-                    index=j;
-                }
-            }
-            if(min==0)
-                break;    
-            arr[index]=-arr[index];
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            sum+=arr[i];
-        }
-        return sum;
-    }
-    
+	    static int largest_sum(int arr[],int n,int k)
+	    {
+		int sum=0;
+		for(int i=1;i<=k;i++)
+		{
+		    int min=arr[0];
+		    int index=0;
+		    for(int j=1;j<n;j++)
+		    {
+			if(arr[j]<min)
+			{
+			    min=arr[j];
+			    index=j;
+			}
+		    }
+		    if(min!=0)   
+		    arr[index]=-arr[index];
+		}
+
+		for(int i=0;i<n;i++)
+		{
+		    sum+=arr[i];
+		}
+		return sum;
+	    }
+
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
@@ -42,7 +41,7 @@ public class q8
 		}
 		System.out.println("Enter the value of k");
 		int k=sc.nextInt();
-		System.out.print("The length of longest subarray is : ");
+		System.out.print("The largest possible sum is : ");
 		System.out.println(largest_sum(arr, n, k));
 	}
 }	
