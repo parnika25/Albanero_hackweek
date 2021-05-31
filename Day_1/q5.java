bbfce488310871560ba9ce6ff199562a79805c9d
Parnika Katiyar <59792325+parnika25@users.noreply.github.com>
2021-05-31 19:44:53 +0530

    Create q5.java

--- Day_1/q5.java
+++ Day_1/q5.java
@@ -3,7 +3,7 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-	    int sum=0;
+		int sum=0;
 		Scanner sc = new Scanner(System.in);
 		int n=sc.nextInt();
 		int arr[] = new int[n];
@@ -15,13 +15,13 @@ public class Main
 		    arr[i]=sc.nextInt();
 		}
 		int x=0,y=0;
-        for(int i=0;i<n;i++)
-        {
-            if((arr[i]% 2) == 0)
-            even[x++] = arr[i];
-            else
-            odd[y++] = arr[i];
-        }
-        System.out.println("The Outliner Number is " + ((even.length > odd.length) ? odd[0] : even[0]));
+	        for(int i=0;i<n;i++)
+	        {
+	            if((arr[i]% 2) == 0)
+	            even[x++] = arr[i];
+	            else
+	            odd[y++] = arr[i];
+	        }
+        	System.out.println("The Outliner Number is " + ((even.length > odd.length) ? odd[0] : even[0]));
 	}
 }	
