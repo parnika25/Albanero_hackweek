c11771b326adb8d88a65654eb77950cef421828a
Parnika Katiyar <59792325+parnika25@users.noreply.github.com>
2021-06-01 03:36:27 +0530

    Create q9.java

---
+++ Day_1/q9.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.Math;
+public class Main 
+{
+
+    int profit(int arr[],int n)
+    {
+            int mini=Integer.MAX_VALUE;
+            int max_p=0;
+            for(int i=0;i<n;i++)
+            {
+                if(arr[i]<mini)
+                mini=arr[i];
+                max_p= Math.max(max_p,(arr[i]-mini));
+                
+            }
+        return max_p;
+    }
+    
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		Main obj=new Main();
+		System.out.println("Enter the total number of elements ");
+		int n=sc.nextInt();
+		int arr[] = new int[n];
+		System.out.println("Enter the elements");
+		for(int i=0;i<n;i++)
+		{
+		    arr[i]=sc.nextInt();
+		}
+		System.out.print("The maximum profit earned is : ");
+		System.out.println(obj.profit(arr, n));
+	}
+}	
