9a9ea0dfa8804b7a6886eae2eb5a46ab8d45510d
Parnika Katiyar <59792325+parnika25@users.noreply.github.com>
2021-06-03 02:39:22 +0530

    Update q8.java

--- Day_3/q8.java
+++ Day_3/q8.java
@@ -1,3 +1,20 @@
+/*Parity bits are used as a very simple checksum to ensure that binary data isn't corrupted during transit. Here's how they work:
+
+  If a binary string has an odd number of 1's, the parity bit is a 1.
+  If a binary string has an even number of 1's, the parity bit is a 0.
+  The parity bit is appended to the end of the binary string.
+  Write a program that adds the correct parity bit to a binary string.
+
+  Examples
+    ("1011011") ➞ "10110111"
+    -- There are five 1's.
+    -- Since five is odd, the parity bit should be a 1.
+    -- Add the parity bit to the end of the string.
+    -- Return the result.
+
+    ("0101101") ➞ "01011010"
+*/
+
 import java.util.*;
 public class q8
 {
