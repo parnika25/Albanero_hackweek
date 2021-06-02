8abdd5d1171dcfdd081527293283ee960ff9e85e
Parnika Katiyar <59792325+parnika25@users.noreply.github.com>
2021-06-03 03:15:18 +0530

    Update q7.java

--- Day_3/q7.java
+++ Day_3/q7.java
@@ -1,3 +1,11 @@
+/* Write a program that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+
+  Examples
+    arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+
+    arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+*/
+
 import java.util.*;
 public class q7
 {
