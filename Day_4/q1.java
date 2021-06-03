ec13c071f58324b26c462e7cb45c8f14b5f5e87b
Parnika Katiyar <59792325+parnika25@users.noreply.github.com>
2021-06-03 17:34:13 +0530

    Update q1.java

--- Day_4/q1.java
+++ Day_4/q1.java
@@ -1,3 +1,11 @@
+/* Write a program  that takes an array of arrays with numbers. Return a new (single) array with the largest numbers of each.
+
+  Examples
+    ([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]) ➞ [-34, -2, 7]
+
+    ([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]]) ➞ [0.7634, 9.32, 9]
+*/
+
 public class q1
 {
 	public static void main(String[] args) 
